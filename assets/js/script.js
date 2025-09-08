e7e8d0b9497d989c89a2d5a76adcf9084e120740
Ariana <aridiaztaafur@crackthecode.com>
2025-09-08 11:24:12 -0500

    enlazando los html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 let name = "techbloom";
 
-console.log("antes de la condicional")
+console.log("antes de la condicional");
 
-if (name.length > 7);
+if (name.length > 7) {
 
-console.log("que nombre tan largo tienes");
+    console.log("que nombre tan largo tienes");
+}
+
+console.log("despues de la condicional");
+
+function showMessages() {
+    console.log("a funcion a sido llamada");
+    console.log("inicio de los mensajes");
+    console.log("bienvenida");
+    console.log(true);
+    console-log(7);
+    console.log("fin de los mensages");
+}
+
+prompt("ingresa tu nombre");
