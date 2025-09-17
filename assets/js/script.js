6978526a8f1e82da393f2d6f7a6e907b3bc484bc
Ariana <aridiaztaafur@crackthecode.com>
2025-09-17 10:59:24 -0500

    mejorando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,83 @@
 let name = "techbloom";
 
-console.log("antes de la condicional");
+console.log("añadimos una condicionn");
 
 if (name.length > 7) {
 
-    console.log("que nombre tan largo tienes");
+    console.log("ingresaste un nombre valido");
 }
 
-console.log("despues de la condicional");
+console.log("despues de la condicionn");
 
+// Añadimos un comentario para ver si funciona el git
 function showMessages() {
-    console.log("a funcion a sido llamada");
+    console.log("la funcion a sido llamada");
     console.log("inicio de los mensajes");
     console.log("bienvenida");
-    console.log(true);
-    console-log(7);
     console.log("fin de los mensages");
 }
 
-prompt("ingresa tu nombre");
+let palabra = "hola mundo";  {
+
+console.log(palabra[0]); // h
+console.log(palabra[1]); // o
+console.log(palabra[2]); // l
+console.log(palabra[3]); // a
+console.log(palabra[4]); // " "
+console.log(palabra[5]); // m  
+console.log(palabra[6]); // u
+console.log(palabra[7]); // n
+console.log(palabra[8]); // d
+console.log(palabra[9]); // o
+
+}
+console.log(palabra.length); // 10
+
+// Accedemos al último carácter
+console.log(palabra[palabra.length - 5]); // m
+
+let saludo = "holaaa";
+
+console.assert(saludo === "holaaa"); // No hay error
+console.assert(saludo === "holaaa"); // sin error
+console.log("despues de la asersion");
+console.log("fin del script");
+
+let form1 = document.forms[0]; // primer formulario
+if (form1) {
+  form1.addEventListener("submit", function(event) {
+    validarRegistro(event);
+  });
+  function validarRegistro(){
+  return false; 
+}
+console.log("decimos que la validacion es correcta");
+}
+
+function validarRegistro(){
+  let usuario = document.formRegistro.usuario.value;
+  let pass = document.formRegistro.pass.value;
+  let valido = true;
+
+  if(usuario.length < 3){
+    document.getElementById("msgUsuario").textContent = "mínimo 3 caracteres";
+    valido = false;
+  } else {
+    document.getElementById("msgUsuario").textContent = "correcto";
+  }
+
+  if(pass.length < 6){
+    document.getElementById("msgPass").textContent = "mínimo 6 caracteres";
+    valido = false;
+  } else {
+    document.getElementById("msgPass").textContent = "correcto";
+  }
+
+  if(valido === true){
+    alert("Registro exitoso");
+  }
+
+  return false;
+}
+
+
