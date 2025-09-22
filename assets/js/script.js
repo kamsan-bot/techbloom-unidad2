7021d83a8e15c969f149a40dd76c4a56be11b724
Luana <luaperezvidal@crackthecode.la>
2025-09-22 08:34:02 -0500

    Subo mi proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,3 +75,18 @@ function animarScroll() {
         }
       });
 
+      document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll('.card');
+  const aparecer = () => {
+    cards.forEach(card => {
+      const rect = card.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100) {
+        card.classList.add('visible');
+      }
+    });
+  };
+  window.addEventListener('scroll', aparecer);
+  aparecer();
+});
+
+
